5b9eebe26e7cd78e83679eac7950a47b449903b7
Your Name <you@example.com>
2024-08-15 13:40:07 +0800

    nihao

--- kernel/vm.c
+++ kernel/vm.c
@@ -322,12 +322,13 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
-    pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+    pa = PTE2PA(*pte); // PA in the old pagetable.
+    *pte = (*pte) & (~PTE_W); // Disable write flag in the pte.
+    flags = PTE_FLAGS(*pte); // Flags in the olad pagetable entry.
+    /* if((mem = kalloc()) == 0)
+         goto err; 
+    memmove(mem, (char*)pa, PGSIZE);*/
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
       kfree(mem);
       goto err;
     }
